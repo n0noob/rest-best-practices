2563be0311f454f9143faab539832d61cc312807
n0noob <neemanoop@gmail.com>
2021-06-09 23:18:21 +0530

    Added get mapping in quotecontroller

--- src/main/java/com/learn/restbestpractices/controllers/QuoteController.java
+++ src/main/java/com/learn/restbestpractices/controllers/QuoteController.java
@@ -1,17 +1,26 @@
 package com.learn.restbestpractices.controllers;
 
+import com.learn.restbestpractices.models.Quote;
 import com.learn.restbestpractices.services.QuoteService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/quotes/v1/")
+@RequestMapping("/quotes/")
 public class QuoteController {
 
     @Autowired
     private QuoteService quoteService;
+
+    @GetMapping(value = "/{id}")
+    public Quote getQuote(@PathVariable long id) {
+        log.info("Received request for quote id : " + id);
+        return quoteService.getQuote(id);
+    }
 
 }
